287565ea0bbc417aa73782c9613d88cd0fd2c389
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-24 06:26:49 -0700

    applying question no 0, 1

--- printf.c
+++ printf.c
@@ -6,17 +6,17 @@
  * _printf - a function that prints everything
  * @format: a pointer to a string to be printed
  * Return: the number of string characters
- */
+*/
 
 int _printf(const char *format, ...)
 {
     int i = 0, prt_sum = 0, sum;
     va_list ap;
 
-    va_start(ap, format);
-    while (format && format[i])
+    va_start (ap, format);
+    while(format && format[i])
     {
-        if (format[i] == '%')
+         if (format[i] == '%')
         {
             prt_sum += get_func(format[i + 1], ap);
             i += 2;
--- printing_functions.c
+++ printing_functions.c
@@ -5,25 +5,24 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+ return (write(1, &c, 1));
 }
 
 /**
  * p_char - a function that prints a single charachter
  * @ap: a pointer to the last fixed argument
  * Return: (1) on sucess, (0) if NULL
- */
+*/
 int p_char(va_list ap)
 {
     char c;
 
-    c = va_arg(ap, int);
+    c =(va_arg(ap, int));
     if (!c)
         return (0);
     _putchar(c);
@@ -34,12 +33,11 @@ int p_char(va_list ap)
  * p_string - a function that print a string
  * @ap: a pointer to the last fixed argument
  * Return: length of the string (len)
- */
+*/
 int p_string(va_list ap)
 {
     char *s = va_arg(ap, char *);
     int len = 0;
-
     if (!s)
         s = "(null)";
     while (*s)
@@ -55,44 +53,55 @@ int p_string(va_list ap)
  * p_decimal - a function that prints a decimal number
  * @ap: a pointer to the last fixed argument
  * Return: length of the string (len)
- */
+*/
 int p_decimal(va_list ap)
-{
-    int n = va_arg(ap, int);
+{ 
     int len = 0;
+    int n = va_arg(ap, int);
 
-    if (n < 0)
-    {
+    if (n < 0) {
         _putchar('-');
         n = -n;
         len++;
     }
-    if (n / 10 != 0)
-        len += p_decimal(ap);
+    len += recursive_print(n);
+   return (len);
+}
+/**
+ * recursive_print - a function to print recursivly
+ * @n: the number to print
+ * @Return: the length of the printed number
+*/
+int recursive_print(int n)
+{
+    int len = 0;
+
+
+    if (n / 10 != 0) {
+        len += recursive_print(n / 10);
+    }
+
     _putchar(n % 10 + '0');
     len++;
+
     return (len);
 }
 
 /**
- * p_integer - a function that prints an integer
+ * p_integer - a function that prints a decimal number
  * @ap: a pointer to the last fixed argument
  * Return: length of the string (len)
- */
+*/
 int p_integer(va_list ap)
 {
+   int len = 0;
     int n = va_arg(ap, int);
-    int len = 0;
 
-    if (n < 0)
-    {
+    if (n < 0) {
         _putchar('-');
         n = -n;
         len++;
     }
-    if (n / 10 != 0)
-        len += p_integer(ap);
-    _putchar(n % 10 + '0');
-    len++;
-    return (len);
+    len += recursive_print(n);
+   return (len);
 }

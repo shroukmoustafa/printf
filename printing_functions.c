97fff824ec00b1b20789d580830b46a3492f4e8b
shroukmoustafa <shroukmoustafa36@gmail.com>
2023-07-26 00:25:02 +0200

    after modification

--- printing_functions.c
+++ printing_functions.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -12,7 +11,6 @@ int _putchar(char c)
 {
  return (write(1, &c, 1));
 }
-
 /**
  * p_char - a function that prints a single charachter
  * @ap: a pointer to the last fixed argument
@@ -21,7 +19,6 @@ int _putchar(char c)
 int p_char(va_list ap)
 {
     char c;
-
     c =(va_arg(ap, int));
     if (!c)
         return (0);
@@ -29,79 +26,4 @@ int p_char(va_list ap)
     return (1);
 }
 
-/**
- * p_string - a function that print a string
- * @ap: a pointer to the last fixed argument
- * Return: length of the string (len)
-*/
-int p_string(va_list ap)
-{
-    char *s = va_arg(ap, char *);
-    int len = 0;
-    if (!s)
-        s = "(null)";
-    while (*s)
-    {
-        _putchar(*s);
-        s++;
-        len++;
-    }
-    return (len);
-}
 
-/**
- * p_decimal - a function that prints a decimal number
- * @ap: a pointer to the last fixed argument
- * Return: length of the string (len)
-*/
-int p_decimal(va_list ap)
-{ 
-    int len = 0;
-    int n = va_arg(ap, int);
-
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-        len++;
-    }
-    len += recursive_print(n);
-   return (len);
-}
-/**
- * recursive_print - a function to print recursivly
- * @n: the number to print
- * @Return: the length of the printed number
-*/
-int recursive_print(int n)
-{
-    int len = 0;
-
-
-    if (n / 10 != 0) {
-        len += recursive_print(n / 10);
-    }
-
-    _putchar(n % 10 + '0');
-    len++;
-
-    return (len);
-}
-
-/**
- * p_integer - a function that prints a decimal number
- * @ap: a pointer to the last fixed argument
- * Return: length of the string (len)
-*/
-int p_integer(va_list ap)
-{
-   int len = 0;
-    int n = va_arg(ap, int);
-
-    if (n < 0) {
-        _putchar('-');
-        n = -n;
-        len++;
-    }
-    len += recursive_print(n);
-   return (len);
-}
---
+++ printing_functions_1.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * p_u_integer - a function that prints an unsigned decimal number
+ * @ap: a pointer to the last fixed argument
+ * Return: length of the string (len)
+*/
+int p_u_integer(va_list ap)
+{
+   int len = 0;
+    unsigned int n = va_arg(ap, unsigned int);
+
+    n = ((int) n);
+    len += recursive_print_u(n);
+   return (len);
+}
+
+/**
+ * recursive_print - a function to print recursivly
+ * @n: the number to print
+ * @Return: the length of the printed number
+*/
+int recursive_print_u(unsigned int n)
+{
+    unsigned int len = 0;
+
+
+    if (n / 10 != 0) {
+        len += recursive_print_u(n / 10);
+    }
+
+    _putchar(n % 10 + '0');
+    len++;
+
+    return (len);
+}
+
